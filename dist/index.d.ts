35a8f7511f2ac19234e1d6d623d946b22fdbbb6c
Toby Twigger <tobytwigger1@gmail.com>
2022-08-18 02:48:05 +0100

    Remove dist

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare type VueOptions = import('./types/vue').VueOptions;
-export declare const installer: {
-    install(VueInstance: any, options: VueOptions): void;
-};
-export default installer;
